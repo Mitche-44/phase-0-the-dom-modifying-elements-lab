b46ed35bfdd1392cb72b6d4b37d8a46ed970b11a
+mitchellengetich <+mitchellngetich24@gmail.com>
2025-03-28 18:29:59 +0300

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
+
+let main = document.querySelector("main#main");
+main.remove();
+
+let newMain = document.createElement("");
+newMain.id = "main";
+document.body.appendChild(newMain);
